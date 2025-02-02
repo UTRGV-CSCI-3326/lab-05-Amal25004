e1223cfb10b7489e65b879f665ab81f19c1f64b9
A. Mal <jmaldonado250@icloud.com>
2025-02-02 05:45:09 +0000

    First commit. must use true or false for smoker question.

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Input
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new  Scanner(System.in);
+
+        String name;
+        int age;
+        double weight;
+        boolean smoker;
+
+        System.out.println("Name: ");
+        name = scanner.nextLine();
+
+        System.out.println("Age: ");
+        age = scanner.nextInt();
+
+        System.out.println("Weight: ");
+        weight = scanner.nextDouble();
+        
+        System.out.println("Smoker: ");
+        smoker = scanner.nextBoolean();
+
+        System.out.println(name + " , " + age + " , " + weight + " , " + smoker);
+    }
+}
